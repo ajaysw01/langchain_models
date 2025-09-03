368cd381dfa1a60a4e141652bc474a802c333e2c
Ajay Wankhade <ajay.wankhade@creditsafe.com>
2025-09-03 14:47:39 +0530

    Text splitters in langchain

---
+++ 10.Lanchain_Text_Splitters/length_based..py
@@ -0,0 +1,14 @@
+from langchain.text_splitter import  CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+loader = PyPDFLoader('Feedback.pdf')
+docs = loader.load()
+
+splitter = CharacterTextSplitter(
+    chunk_size=100,
+    chunk_overlap=0,
+    separator=''
+)
+
+# result = splitter.split_text(text) for normal text
+result = splitter.split_documents(docs)
+print(result[0])
